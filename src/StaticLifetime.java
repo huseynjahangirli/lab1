cceb935b5571da0ece51b00c9e219cc1f053fac8
huseyincihangirli <cihangirlihuseyin68@gmail.com>
2025-10-09 09:48:32 +0300

    init commit

---
+++ src/StaticLifetime.java
@@ -0,0 +1,21 @@
+public class StaticLifetime {
+
+    // static değişken
+    static int counter = 0;
+
+    // metot: counter'ı artır ve yazdır
+    void incrementCounter() {
+        counter++;
+        System.out.println("Counter: " + counter);
+    }
+
+    public static void main(String[] args) {
+        StaticLifetime obj1 = new StaticLifetime();
+        StaticLifetime obj2 = new StaticLifetime();
+
+        obj1.incrementCounter(); // Counter: 1
+        obj2.incrementCounter(); // Counter: 2 (çünkü static)
+    }
+}
+
+
